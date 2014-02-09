f1f5420619ee60b04252745fc0d6892964ea4931
Connor Williams <jricacosta+github@gmail.com>
2014-02-09 00:23:53 -0800

    continue work on argv parsing

--- lib/cli_utils.rb
+++ lib/cli_utils.rb
@@ -5,31 +5,54 @@ end
 
 class CliUtils
   include Errors
-  attr_accessor :args,:command,:config
+  attr_accessor :options,:required,:command,:config
 
   def initialize(config_filepath=nil, commands_filepath=nil)
     init_commands(commands_filepath)
-    parse_args
+    parse_options
     init_config(filepath=nil)
   end
 
   def init_commands(commands_filepath)
     #TODO
-    @commands = {}
+    @commands = {'foo'=> {'required' => ['bar','baz']}}
   end
 
-  def parse_args
-    @args = {}
+  def parse_options
+    @options = {}
     ARGV.each_with_index {|arg, i|
+      next_arg = ARGV[i + 1]
+
       if arg[0] == '-'
-        if arg[1] == '-'
-          raise ParseError unless ARGV[i + 1]
-          #TODO warn when the value of --foo is a command
-          @args[arg[2..-1]] = processValue(ARGV[i + 1])
+        if next_arg && !(is_command?(next_arg) || next_arg.start_with?('-'))
+          @options[arg.gsub(/\A-+/,'')] = processValue(next_arg)
         else
+          @options[arg.gsub(/\A-+/,'')] = true
+        end
+      else
+        next if @command
+
+        if is_command?(arg)
+          @command = arg
+          req_keys = (@commands[@command]['required'] || [])
+          req_vals = ARGV[i + 1, req_keys.length]
+
+          raise ParseError unless req_keys.length == req_vals.length
+          raise ParseError if req_vals.map{|v| v.chr}.include?('-')
+
+          @required = {}
+          req_keys.zip(req_vals).each{|(k,v)| @required[k] = processValue(v)}
         end
       end
     }
+  end
+
+  def is_command?(str)
+    (@commands || {}).has_key?(str)
+  end
+
+  def tail(arr)
+    arr[1..-1]
   end
 
   def init_config(config_filepath)
@@ -43,4 +66,5 @@ class CliUtils
   end
 end
 
-puts CliUtils.new.inspect
+c = CliUtils.new
+puts c.inspect
